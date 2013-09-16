938efd690e0dde8aa914d3a68909bd5693914758
Leonardo Varuzza <varuzza@gmail.com>
2013-09-16 13:09:35 -0300

    Add insertStats code

---
+++ BAMFolder/src/main/java/bfx/assembly/scaffold/edges/InsertStats.java
@@ -0,0 +1,24 @@
+package bfx.assembly.scaffold.edges;
+
+import net.sf.samtools.SAMRecord;
+import bfx.assembly.scaffold.bam.AlignConsumer;
+
+public class InsertStats extends AlignConsumer {
+	//private static Logger log = LoggerFactory.getLogger(BAMReader.class);
+	
+	public InsertStats() {
+	}
+
+	@Override
+	public void callback(SAMRecord aln) {}
+
+	@Override
+	public void start() {
+	}
+
+	@Override
+	public void finish() {
+	}
+
+
+}
